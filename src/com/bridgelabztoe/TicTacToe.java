3507040859abaec095a4db033bb2dab94751d1f9
sudha123 <sudhanaukala1997@gmail.com>
2022-07-17 17:24:40 +0530

    show board

--- src/com/bridgelabztoe/TicTacToe.java
+++ src/com/bridgelabztoe/TicTacToe.java
@@ -17,4 +17,8 @@ public class TicTacToe {
             board[i]=' ';
         }
     }
+ static void showBoard(char[] board){
+    System.out.println(board[1]+"|"+board[2]+"|"+board[3]);
+    System.out.println(board[4]+"|"+board[5]+"|"+board[6]);
+    System.out.println(board[7]+"|"+board[8]+"|"+board[9]);
 }
